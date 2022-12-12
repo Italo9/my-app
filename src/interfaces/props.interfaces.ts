b4b7597e397151528255efe368112fe47a6f3705
Italo <contatoitallo@hotmail.com>
2022-12-12 07:42:28 -0300

    feat: :sparkles: Cria interface para props

---
+++ src/interfaces/props.interfaces.ts
@@ -0,0 +1,24 @@
+import { InputHTMLAttributes, ReactNode } from "react";
+import {
+  FieldError,
+  FieldErrorsImpl,
+  FieldValues,
+  Merge,
+  UseFormRegister,
+} from "react-hook-form";
+
+export interface Ichildren {
+  children: ReactNode;
+}
+
+export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+  register: UseFormRegister<FieldValues>;
+  placeholder: string;
+  error?:
+    | string
+    | FieldError
+    | Merge<FieldError, FieldErrorsImpl<any>>
+    | undefined;
+}
